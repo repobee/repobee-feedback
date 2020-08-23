dd75d11064eb52e89fbfdbdb468e15a137854fb5
Simon Larsén <slarse@slar.se>
2020-08-23 17:04:55 +0200

    [misc] Require Python 3.6+, #7

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     extras_require=dict(TEST=test_requirements),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Education",
